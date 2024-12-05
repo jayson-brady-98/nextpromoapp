a5da12596f56d8602bdda0adc96485357d710e47
jayson-brady-98 <jaysonbrady123@gmail.com>
2024-12-05 17:22:02 +1100

    Removed the static params since it seemed unnecessary

--- app/ad/[brand]/page.tsx
+++ app/ad/[brand]/page.tsx
@@ -6,12 +6,6 @@ type AdPageProps = {
   }
 }
 
-export async function generateStaticParams() {
-  // Replace with your logic to fetch available brands
-  const brands = ['brand1', 'brand2', 'brand3'] // Example brands
-  return brands.map((brand) => ({ brand }))
-}
-
 export default function AdPage({ params }: AdPageProps) {
   return <AdDisplay brand={params.brand} />
 }
