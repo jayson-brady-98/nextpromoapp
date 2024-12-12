e5a376168b2441e753988d49ad6bd1fdbc03bba8
jayson-brady-98 <jaysonbrady123@gmail.com>
2024-12-13 07:57:01 +1100

    Current full working version. No longer beta

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,6 @@ const HomePage = () => {
                 <h1 className="text-6xl md:text-6.5xl font-bold text-[#E84753]">
                   nextpromo.io
                 </h1>
-                <span className="absolute top-[-15px] right-[-20px] text-xs font-semibold text-white bg-red-500 rounded-full px-2 py-1">
-                  Beta
-                </span>
               </div>
               <h2 className="text-sm md:text-base font-medium text-[#D2CAA6] absolute left-[45%] top-[100%] -mt-3">
                 Empowering Consumers
