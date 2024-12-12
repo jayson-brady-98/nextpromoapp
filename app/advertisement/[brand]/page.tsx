d07a3a0e4e2e256c9fe82075d9751ef434d24eb1
jayson-brady-98 <jaysonbrady123@gmail.com>
2024-12-12 18:14:11 +1100

    debugging ad page

--- app/advertisement/[brand]/page.tsx
+++ app/advertisement/[brand]/page.tsx
@@ -6,10 +6,17 @@ type BrandParams = Promise<{ brand: string }>
 
 // Update validation to check predictions table
 async function validateBrand(brand: string) {
+  console.log('AdvertisementPage - Validating brand:', brand)
   const prediction = await fetchPredictions(brand)
+  console.log('AdvertisementPage - Received prediction:', prediction)
+  console.log('AdvertisementPage - Type of prediction:', typeof prediction)
+  console.log('AdvertisementPage - Is prediction truthy?:', !!prediction)
+  
   if (!prediction) {
+    console.log('AdvertisementPage - Redirecting to results')
     redirect(`/results/${brand}`)
   }
+  console.log('AdvertisementPage - Validation passed')
 }
 
 export default async function AdvertisementPage(props: { params: BrandParams }) {
